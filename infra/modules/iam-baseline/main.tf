b3d1cc0df1ddb6b44721a77e496e6ee760855053
Rich <thunninisec@gmail.com>
2026-02-02 19:07:17 -0500

    Create main.tf

---
+++ infra/modules/iam-baseline/main.tf
@@ -0,0 +1,11 @@
+resource "aws_iam_account_password_policy" "strong" {
+  minimum_password_length        = 14
+  require_lowercase_characters   = true
+  require_uppercase_characters   = true
+  require_numbers                = true
+  require_symbols                = true
+  allow_users_to_change_password = true
+  hard_expiry                    = false
+  max_password_age               = 90
+  password_reuse_prevention      = 24
+}
